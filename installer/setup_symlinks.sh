94b95b665b9c25ec4ba0e5147c1d74e701b3a924
naoHash <n.hashimoto.k@gmail.com>
2025-08-15 22:23:46 +0900

    setup_symlinks.sh: コードをリファクタリングし、可読性・堅牢性を向上

--- installer/setup_symlinks.sh
+++ installer/setup_symlinks.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# ===== シンボリックリンク作成処理 =====
 printf '\033[32m%s\033[m\n' '========================'
 printf '\033[32m%s\033[m\n' ' Setting up symlinks... '
 printf '\033[32m%s\033[m\n' '========================'
 
-DOT_DIR_SOURCE="$HOME/dev/github.com/naoHash/dotfiles_pub"
+DOTFILES_DIR="$HOME/dev/github.com/naoHash/dotfiles_pub"
+CONFIG_SRC="$DOTFILES_DIR/.config"
+CONFIG_DEST="$HOME/.config"
 
-# dotfiles
-ln -snf $DOT_DIR_SOURCE ~
+# dotfilesリポジトリ自体をホーム直下にリンク
+ln -snf "$DOTFILES_DIR" ~
 
-# .config
-mkdir -p $HOME/.config
-cd ${DOT_DIR_SOURCE/.config}
-echo $PWD
-dirs=$(find $DOT_DIR_SOURCE/.config -maxdepth 1 -mindepth 1 -type d)
-for dir in $dirs;
-do
-  echo "Creating symbolic link: $dir"
-  ln -snf $dir $XDG_CONFIG_HOME
+# ~/.configディレクトリがなければ作成
+mkdir -p "$CONFIG_DEST"
+
+# .config配下の各ディレクトリを~/.configにリンク
+cd "$CONFIG_SRC"
+for dir in "$CONFIG_SRC"/*/; do
+  # ディレクトリのみ対象
+  [ -d "$dir" ] || continue
+  echo "Creating symbolic link: $dir -> $CONFIG_DEST"
+  ln -snf "$dir" "$CONFIG_DEST"
 done
